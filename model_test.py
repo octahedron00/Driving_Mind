3d75cc07fbf53641248a53f9423a31ef8b8fe3e6
Octo <octahedron00@gmail.com>
2024-11-25 13:55:27 +0900

    model testing code is built

--- model_test.py
+++ model_test.py
@@ -1,17 +1,67 @@
 # -*- coding: utf-8 -*-
 
 
-import cv2
 import numpy as np
 import time
-import math
-import datetime
-from multiprocessing import Process, Manager
 
 from ultralytics import YOLO, RTDETR
 
-from src.fake_tiki import TikiMini
 
-from src._lane_detect import get_bev, get_road, get_sliding_window_result, get_green, get_square_pos, Line
-from src._mode import StartMode, EventMode, Stanley2GreenMode, Stanley2CrossMode, Turn2VoidMode, Turn2RoadMode, EndMode
-from src._model_second import run_model_second
+
+
+# 마음껏 수정하며 진행할 것.
+
+DO_SECOND = False
+
+
+FILE_1 = "rtdetr-l.pt"
+FILE_1_TRT = "rtdetr-l.onnx"
+FILE_2 = "yolo11l.pt"
+FILE_2_TRT = "yolo11l.engine"
+
+IMG_SIZE = (640, 640, 3)
+IMG_TEST = np.zeros(IMG_SIZE)
+
+print("All import finished")
+
+
+start_time = time.time()
+model_1 = RTDETR(FILE_1)
+model_1.to('cpu')
+# model_1.to('cuda')
+print(f"Model 1 Starting: {time.time()-start_time:.03f} seconds")
+
+# model_1.export(format="onnx")
+
+# model_1 = RTDETR(FILE_1_TRT)
+
+if DO_SECOND:
+    start_time = time.time()
+    model_2 = YOLO(FILE_2)
+    # model_2.to('cuda')
+    print(f"Model 2 Starting: {time.time()-start_time:.03f} seconds")
+
+
+
+start_time = time.time()
+model_1.predict(IMG_TEST)
+print(f"Model 1 First inference: {time.time()-start_time:.03f} seconds")
+
+if DO_SECOND:
+    start_time = time.time()
+    model_2.predict(IMG_TEST)
+    print(f"Model 2 First inference: {time.time()-start_time:.03f} seconds")
+
+
+
+for i in range(5):
+
+    start_time = time.time()
+    model_1.predict(IMG_TEST)
+    print(f"Model 1 RealTime inference: {time.time()-start_time:.03f} seconds")
+
+    if DO_SECOND:
+        start_time = time.time()
+        model_2.predict(IMG_TEST)
+        print(f"Model 2 Second inference: {time.time()-start_time:.03f} seconds")
+    print()
