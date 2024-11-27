9fd9411b73ad641d8e843cb36bf13948d0b34b8c
Octo <octahedron00@gmail.com>
2024-11-28 00:46:54 +0900

    stanley debug

--- src/_mode.py
+++ src/_mode.py
@@ -123,10 +123,10 @@ def move_stanley(pub, offset_mm, angle_deg, x_ratio=1):
 
     kp = 0.05
     ka = 0.10
-    k = 0.6
+    k = 1.5
     x = SPEED_X * x_ratio
 
-    z = -(angle_deg * ka - math.atan(kp * offset_mm)) * k * x
+    z = -(angle_deg * ka - math.atan(kp * offset_mm)) * k * x * x 
 
     move_robot(pub, x, z)
 
