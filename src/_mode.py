af5a2fe9ec95c80ddd1ff882b28b203780ffcd1c
Octo Moon <75821497+octahedron00@users.noreply.github.com>
2024-11-29 03:59:11 +0900

    update better stanley

--- src/_mode.py
+++ src/_mode.py
@@ -96,9 +96,9 @@ def move_robot(pub, vel_x=0, rot_z=0, is_left=True):
 
 def move_stanley(pub, offset_mm, angle_deg, x_ratio=1):
 
-    kp = 0.05
-    ka = 0.10
-    k = 2
+    kp = 0.03 # 0.05
+    ka = 0.12 # 0.10
+    k = 2.0
     x = SPEED_X * x_ratio
 
     z = -(angle_deg * ka - math.atan(kp * offset_mm)) * k * x * x 
