d2572c8c7bc6f943505b261ef03d9132d524fb98
Octo <octahedron00@gmail.com>
2024-11-28 10:17:47 +0900

    no more useless things

--- mind_bot.py
+++ mind_bot.py
@@ -134,8 +134,8 @@ class Bot_Mind:
                 self.model_each = YOLO(FILE_EACH)
             self.model_each.to('cuda')
             null_predict_to_turn_on = self.model_each.predict(np.zeros((640, 640, 3)), device=0)
-            null_predict_to_turn_on = self.model_each.predict(np.zeros((640, 640, 3)), device=0)
-            null_predict_to_turn_on = self.model_each.predict([np.zeros((640, 640, 3))]*5, device=0)
+            # null_predict_to_turn_on = self.model_each.predict(np.zeros((640, 640, 3)), device=0)
+            # null_predict_to_turn_on = self.model_each.predict([np.zeros((640, 640, 3))]*5, device=0)
         else:
             self.model_each = None
 
