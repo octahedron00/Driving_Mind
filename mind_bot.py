90b979106bb5d407e987ec49418fc035fb32b03e
Octo <octahedron00@gmail.com>
2024-12-10 17:12:13 +0900

    cross adjusting / T2V twice

--- mind_bot.py
+++ mind_bot.py
@@ -180,7 +180,8 @@ class Bot_Mind:
             Stanley2CrossMode(pub, 0, speeding_time=0.0, prefer_dist=220),
             Turn2RoadMode(pub, 2,       is_left=False,  is_curve=True, z_ratio=0.3),
             Stanley2CrossMode(pub, 0, speeding_time=1.0, prefer_dist=220),
-            Turn2RoadMode(pub, 2,       is_left=True, min_turn_sec=2.2),
+            Turn2VoidMode(pub, 4,       is_left=True),
+            Turn2VoidMode(pub, 4,       is_left=True),
             _SheepMode(pub, -1, sleep_sec = 1, x_speed = -0.12),
             EndMode(pub, None, 0),
 
